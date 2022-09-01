cd72f65acdd472b7018f412766c5762fed454be6
har5hcodes <yadaiah8550@gmail.com>
2022-09-01 11:53:24 +0530

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,5 @@
+//  profile type/option button components goes here
+// after the user selects (learner or instructor), redirect to (learner-otp-verification, instructor-otp-verification) page
+
+// save the profile-type for further usage
+// if user login state is true then we can take to their home page without asking what profile type they want
---
+++ src/pages/learner-side/LearnerOtpVerificaiton.js
@@ -0,0 +1,5 @@
+// here goes a phone number field and a send otp button
+// in this same page we can have the otp box field 
+// after verifying otp, we can redirect him to the profile details filling form
+
+// if the user has already has an account with that phone number, send him to the their home page 
